610e0fc9221a12c857a1654099dba926d0e7506c
Titas Milan <titas9x@gmail.com>
2011-10-29 14:39:08 +0530

    Done with static pages

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'sqlite3'
 
 # Gems used only for assets and not required
 # in production environments by default.
+
 group :assets do
   gem 'sass-rails',   '~> 3.1.4'
   gem 'coffee-rails', '~> 3.1.1'
@@ -23,6 +24,7 @@ end
 group :test do
 	gem 'rspec', '2.7.0'
 	gem 'webrat', '0.7.3'
+	
 end
 
 gem 'jquery-rails'
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right tilte" do
+      get 'home'
+      response.should have_selector("title", :content => "Myshaadi.com | Home")
+    end    
   end
 
   describe "GET 'contact'" do
@@ -14,6 +20,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right tilte" do
+      get 'contact'
+      response.should have_selector("title", :content => "Myshaadi.com | Contact")
+    end
   end
-
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right tilte" do
+      get 'about'
+      response.should have_selector("title", :content => "Myshaadi.com | About")
+    end
+  end
 end
