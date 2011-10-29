c321dbc141c5713238dd95380cfc893657ab3c34
Titas Milan <titas9x@gmail.com>
2011-10-29 16:36:25 +0530

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before (:each) do
+    @base_title ="Myshaadi.com"
+  end
+  
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -11,7 +16,7 @@ describe PagesController do
     
     it "should have the right tilte" do
       get 'home'
-      response.should have_selector("title", :content => "Myshaadi.com | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end    
   end
 
@@ -23,7 +28,7 @@ describe PagesController do
     
     it "should have the right tilte" do
       get 'contact'
-      response.should have_selector("title", :content => "Myshaadi.com | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
   
@@ -35,7 +40,19 @@ describe PagesController do
     
     it "should have the right tilte" do
       get 'about'
-      response.should have_selector("title", :content => "Myshaadi.com | About")
+      response.should have_selector("title", :content => @base_title + " | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right tilte" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
     end
   end
 end
