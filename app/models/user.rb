140e85ece5a4d166858dc5615395f2f03ea8900f
Titas Milan <titas9x@gmail.com>
2011-11-01 12:30:24 +0530

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
     user = find_by_email(email)
     return nil if user.nil?
     return user if user.has_password?(submitted_password)
-  end
+  end 
   
   private
   
